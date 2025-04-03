68ea03966c7b2e6a3e64bf8d158ba0b9352e99e1
semihpolat <62581780+semihpolat@users.noreply.github.com>
2025-04-03 14:37:19 -0700

    refactor1

--- .tmp/pages/blog/hello-world.js
+++
@@ -1,8 +0,0 @@
-
-        import ContentPage from '../../../src/lib/components/ContentPage.svelte';
-        
-        new ContentPage({
-          target: document.body,
-          props: {"title":"Hello World","content":"<h1>Hello World</h1>\n<p>Welcome to our first blog post. This is a sample markdown file that will be converted to HTML and displayed on our site.</p>\n<h2>Markdown Features</h2>\n<p>Markdown is a lightweight markup language that allows you to write using an easy-to-read, easy-to-write plain text format, which then converts to valid HTML for viewing on a website.</p>\n<h3>Some examples:</h3>\n<ul>\n<li><strong>Bold text</strong> is created using double asterisks</li>\n<li><em>Italic text</em> is created using single asterisks</li>\n<li><a href=\"https://example.com\">Links</a> are created using square brackets and parentheses</li>\n</ul>\n<h3>Code blocks</h3>\n<pre><code class=\"language-javascript\">// This is a code block\nfunction helloWorld() {\n  console.log(&quot;Hello, world!&quot;);\n}\n</code></pre>\n<h2>Next Steps</h2>\n<p>In future posts, we&#39;ll explore more advanced features and integrations with our static site generator. </p>\n","date":"2025-03-24T00:00:00.000Z","author":"John Doe","backLink":"/blog","backLinkText":"Blog","activePath":"/blog/hello-world","currentYear":2025,"navbarItems":[{"title":"Blog","url":"/blog"},{"title":"Docs","url":"/docs"},{"title":"Pages","url":"/"}]}
-        });
-      
--- .tmp/pages/docs/getting-started.js
+++
@@ -1,8 +0,0 @@
-
-        import ContentPage from '../../../src/lib/components/ContentPage.svelte';
-        
-        new ContentPage({
-          target: document.body,
-          props: {"title":"Getting Started","content":"<h1>Getting Started</h1>\n<p>This guide will help you get started with our platform. Follow these simple steps to set up your environment and begin development.</p>\n<h2>Prerequisites</h2>\n<p>Before you begin, make sure you have the following installed:</p>\n<ul>\n<li>Node.js (version 16 or higher)</li>\n<li>npm or yarn</li>\n<li>Git</li>\n</ul>\n<h2>Installation</h2>\n<p>Clone the repository and install dependencies:</p>\n<pre><code class=\"language-bash\">git clone https://github.com/yourusername/your-project.git\ncd your-project\nnpm install\n</code></pre>\n<h2>Configuration</h2>\n<p>Create a <code>.env</code> file in the root directory with the following variables:</p>\n<pre><code>API_KEY=your_api_key\nBASE_URL=https://api.example.com\n</code></pre>\n<h2>Running the Development Server</h2>\n<p>Start the development server with:</p>\n<pre><code class=\"language-bash\">npm run dev\n</code></pre>\n<p>Your site should now be running at <code>http://localhost:5173</code>. </p>\n","backLink":"/docs","backLinkText":"Documentation","activePath":"/docs/getting-started","currentYear":2025,"navbarItems":[{"title":"Blog","url":"/blog"},{"title":"Docs","url":"/docs"},{"title":"Pages","url":"/"}]}
-        });
-      
