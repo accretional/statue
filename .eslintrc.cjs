c67de1576f3bb80fe5d621c20d9f4520f2fb447b
Ben Fultz <benfultz@accretional.com>
2025-12-17 16:26:40 -0800

    Fix linting rules for existing scripts and components

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,13 +14,34 @@ module.exports = {
 	rules: {
 		// Warnings only for now - we don't want to break existing code
 		'no-console': 'warn',
-		'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+		'no-unused-vars': ['warn', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
 		'no-undef': 'warn'
 	},
 	overrides: [
 		{
 			files: ['*.svelte'],
-			parser: 'svelte-eslint-parser'
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			},
+			rules: {
+				// {@html} is intentional for markdown rendering
+				'svelte/no-at-html-tags': 'warn',
+				// Unused exports are fine in components (used externally)
+				'svelte/valid-compile': 'warn',
+			}
+		},
+		{
+			// Allow console in CLI scripts and build tools
+			files: [
+				'scripts/**/*.js',
+				'postinstall.js',
+				'test/**/*.js',
+				'*.config.js'
+			],
+			rules: {
+				'no-console': 'off'
+			}
 		}
 	],
 	ignorePatterns: [
