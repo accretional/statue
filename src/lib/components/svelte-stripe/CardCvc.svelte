96b79f604c13a13bdbe38cc499c2155cf587fad6
brantfeick <brant.feick@gmail.com>
2026-03-11 11:54:25 -0700

    Add svelte-stripe components and @stripe/stripe-js dependency

---
+++ src/lib/components/svelte-stripe/CardCvc.svelte
@@ -0,0 +1,88 @@
+<!--
+This is a Svelte component from svelte-stripe
+
+Website: [sveltestripe.com](https://www.sveltestripe.com/)
+GitHub Repository: [joshnuss/svelte-stripe](https://github.com/joshnuss/svelte-stripe)
+
+All components in this directory are sourced from the svelte-stripe project by joshnuss. Please refer to the original repository for documentation, examples, and additional components.
+-->
+
+<script lang="ts">
+  import type {
+    StripeCardCvcElementOptions as Options,
+    StripeCardCvcElement as Element,
+    StripeCardCvcElementChangeEvent as ChangeEvent,
+    Stripe,
+    StripeElements
+  } from '@stripe/stripe-js'
+  import { getContext, onMount } from 'svelte'
+
+  interface ElementsContext {
+    elements: StripeElements
+    stripe: Stripe
+  }
+
+  type Events = {
+    onchange?: (event: ChangeEvent) => any
+    onready?: (event: { elementType: 'cardCvc' }) => any
+    onfocus?: (event: { elementType: 'cardCvc' }) => any
+    onblur?: (event: { elementType: 'cardCvc' }) => any
+    onescape?: (event: { elementType: 'cardCvc' }) => any
+  }
+
+  type Bindables = {
+    element?: Element
+  }
+
+  type Props = Options & Events & Bindables
+
+  let {
+    element = $bindable(),
+    onchange = () => {},
+    onready = () => {},
+    onfocus = () => {},
+    onblur = () => {},
+    onescape = () => {},
+    ...options
+  }: Props = $props()
+
+  let wrapper = $state<HTMLElement>()
+
+  const { elements }: ElementsContext = getContext('stripe')
+
+  onMount(() => {
+    element = elements.create('cardCvc', options)
+
+    element.on('change', onchange)
+    element.on('ready', onready)
+    element.on('focus', onfocus)
+    element.on('blur', onblur)
+    element.on('escape', onescape)
+
+    element.mount(wrapper!)
+
+    return () => element?.destroy()
+  })
+
+  $effect(() => {
+    element?.update(options)
+  })
+
+  export function blur() {
+    element?.blur()
+  }
+
+  export function clear() {
+    element?.clear()
+  }
+
+  export function destroy() {
+    element?.destroy()
+  }
+
+  export function focus() {
+    element?.focus()
+  }
+</script>
+
+<div bind:this={wrapper}></div>
